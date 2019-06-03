a828934b637f2665cc348285a0422520588ca05d
Unknown <s094179@ap.be>
2019-06-03 23:55:03 +0200

    Final commit for deadline
    Finished & API op Google's MySQL

--- api/cloudAPIs_RESTAPI/cloudAPIs_RESTAPI/Model/DBInitializer.cs
+++ api/cloudAPIs_RESTAPI/cloudAPIs_RESTAPI/Model/DBInitializer.cs
@@ -32,8 +32,149 @@ namespace cloudAPIs_RESTAPI.Model
                     Population = 17308133
                 };
 
+                var country3 = new Country()
+                {
+                    Name = "France",
+                    ISO = "fr",
+                    Capital = "Paris",
+                    Surface_area = 640679,
+                    Population = 67348000
+                };
+
+                var country4 = new Country()
+                {
+                    Name = "Luxembourg",
+                    ISO = "lu",
+                    Capital = "Luxembourg",
+                    Surface_area = 2586,
+                    Population = 602005
+                };
+
+                var country5 = new Country()
+                {
+                    Name = "Germany",
+                    ISO = "de",
+                    Capital = "Berlin",
+                    Surface_area = 357386,
+                    Population = 83000000
+                };
+
+                var country6 = new Country()
+                {
+                    Name = "United Kingdom",
+                    ISO = "gb",
+                    Capital = "London",
+                    Surface_area = 242495,
+                    Population = 66921307
+                };
+
+                var country7 = new Country()
+                {
+                    Name = "Australia",
+                    ISO = "au",
+                    Capital = "Canberra",
+                    Surface_area = 7692024,
+                    Population = 25380900
+                };
+
+                var country8 = new Country()
+                {
+                    Name = "Russia",
+                    ISO = "ru",
+                    Capital = "Moscow",
+                    Surface_area = 17125191,
+                    Population = 146793744
+                };
+
+                var country9 = new Country()
+                {
+                    Name = "United States",
+                    ISO = "us",
+                    Capital = "Washington D.C.",
+                    Surface_area = 9833520,
+                    Population = 327167434
+                };
+
+                var country10 = new Country()
+                {
+                    Name = "Brazil",
+                    ISO = "br",
+                    Capital = "Brasilia",
+                    Surface_area = 8515767,
+                    Population = 210147125
+                };
+
+                var country11 = new Country()
+                {
+                    Name = "South Africa",
+                    ISO = "za",
+                    Capital = "Cape Town",
+                    Surface_area = 1221037,
+                    Population = 57725600
+                };
+
+                var country12 = new Country()
+                {
+                    Name = "India",
+                    ISO = "in",
+                    Capital = "New Delhi",
+                    Surface_area = 3287263,
+                    Population = 1324171354
+                };
+
+                var country13 = new Country()
+                {
+                    Name = "China",
+                    ISO = "cn",
+                    Capital = "Beijing",
+                    Surface_area = 9596961,
+                    Population = 1403500365
+                };
+
+                var country14 = new Country()
+                {
+                    Name = "United Arab Emirates",
+                    ISO = "ae",
+                    Capital = "Abu Dhabi",
+                    Surface_area = 83600,
+                    Population = 9599353
+                };
+
+                var country15 = new Country()
+                {
+                    Name = "Egypt",
+                    ISO = "eg",
+                    Capital = "Cairo",
+                    Surface_area = 1010408,
+                    Population = 94798927
+                };
+
+                var country16 = new Country()
+                {
+                    Name = "Singapore",
+                    ISO = "sg",
+                    Capital = "Singapore",
+                    Surface_area = 723,
+                    Population = 5638700
+                };
+
+
                 context.Countries.Add(country1);
                 context.Countries.Add(country2);
+                context.Countries.Add(country3);
+                context.Countries.Add(country4);
+                context.Countries.Add(country5);
+                context.Countries.Add(country6);
+                context.Countries.Add(country7);
+                context.Countries.Add(country8);
+                context.Countries.Add(country9);
+                context.Countries.Add(country10);
+                context.Countries.Add(country11);
+                context.Countries.Add(country12);
+                context.Countries.Add(country13);
+                context.Countries.Add(country14);
+                context.Countries.Add(country15);
+                context.Countries.Add(country16);
                 context.SaveChanges();
             }
         }
--- api/cloudAPIs_RESTAPI/cloudAPIs_RESTAPI/Startup.cs
+++ api/cloudAPIs_RESTAPI/cloudAPIs_RESTAPI/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using cloudAPIs_RESTAPI.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -21,17 +22,30 @@ namespace cloudAPIs_RESTAPI
             Configuration = configuration;
         }
         
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LibraryContext>(
+            /*services.AddDbContext<LibraryContext>(
                 options => options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")
                 )
+            );*/
+
+            services.AddDbContext<LibraryContext>(
+                options => options.UseMySql(
+                    Configuration.GetConnectionString("DefaultConnection")
+                )
             );
+
+            // Authentication info
+            /*services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://accounts.google.com";
+                    options.Audience = "client-id";
+                });*/
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -49,6 +63,7 @@ namespace cloudAPIs_RESTAPI
                        .AllowAnyMethod()
                        .AllowAnyHeader());
 
+            //app.UseAuthentication();
             app.UseMvc();
 
             DBInitializer.Initialize(libContext);
